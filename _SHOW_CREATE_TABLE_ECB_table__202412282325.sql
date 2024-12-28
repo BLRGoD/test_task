afdd4237cc97a1e8b19a4c726ae52586fd811b19
Ivan Ahramovich <35844661+BLRGoD@users.noreply.github.com>
2024-12-28 23:26:47 +0300

    Add files via upload

---
+++ _SHOW_CREATE_TABLE_ECB_table__202412282325.sql
@@ -0,0 +1,18 @@
+INSERT INTO `SHOW CREATE TABLE ECB_table` (`statement`) VALUES
+	 ('CREATE TABLE default.ECB_table
+(
+    `ID` LowCardinality(String),
+    `NumCode` LowCardinality(String),
+    `CharCode` LowCardinality(String),
+    `Nominal` String,
+    `Name` String,
+    `Value` Float64,
+    `X_to_USD` Float64,
+    `USD_to_X` Float64,
+    `X_to_EUR` Float64,
+    `EUR_to_X` Float64,
+    `ExecutionTime` DateTime
+)
+ENGINE = ReplacingMergeTree
+ORDER BY (CharCode, ID)
+SETTINGS index_granularity = 8192');
